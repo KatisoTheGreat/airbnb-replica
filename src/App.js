e654e0f7034efb0a375710f57d55c9dc57e17345
KatisoTheGreat <katisoledwaba@gmail.com>
2023-06-03 20:47:22 +0200

    added props on component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import Navbar from "../src/components/Navbar";
 import Hero from "../src/components/Hero";
 import Card from "../src/components/Card";
+import card from "../src/images/katie-zaferes.png"
 
 function App() {
     return(
         <div>
         <Navbar />
         {/* <Hero /> */}
-        <Card />
+        <Card 
+            img={card}
+            rating="5.0"
+            reviewCount={(6)}
+            country="USA"
+            title="Life Lessons with Katie Zaferes"
+            price={136}
+        />
     </div>
     )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
-import card from "../images/katie-zaferes.png"
 import star from "../images/star.png"
 
-function Card() {
+function Card(props) {
     return(
         <div className="card">
-            <img src={card} alt="" className="card-image"/>
+            <img src={props.img} alt="" className="card-image"/>
             <div className="card-stats">
                 <img src={star} alt="" className="card-star"/>
-                <span>5.0</span>
-                <span className="grey">(6) -</span>
-                <span className="grey">USA</span>
+                <span>{props.rating}</span>
+                <span className="grey">{props.reviewCount} -</span>
+                <span className="grey">{props.country}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><b>From $136</b> / person</p>
+            <p>{props.title}</p>
+            <p><b>From ${props.price}</b> / person</p>
         </div>
     )
 }
