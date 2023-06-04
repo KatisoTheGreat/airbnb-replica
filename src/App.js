5fd4d24e018204e183f9c9a159b326b397570859
KatisoTheGreat <katisoledwaba@gmail.com>
2023-06-04 17:58:19 +0200

    mapped data into card component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import React from "react";
 import Navbar from "../src/components/Navbar";
 import Hero from "../src/components/Hero";
 import Card from "../src/components/Card";
-import card from "../src/images/katie-zaferes.png"
+import data from "../src/data"
 
 function App() {
+
+    const cards = data.map(item => {
+        return(
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
+
     return(
         <div>
         <Navbar />
         {/* <Hero /> */}
-        <Card 
-            img={card}
-            rating="5.0"
-            reviewCount={6}
-            country="USA"
-            title="Life Lessons with Katie Zaferes"
-            price={136}
-        />
+        {cards}
     </div>
     )
 }
