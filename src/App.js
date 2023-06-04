b66a5ca4064e2672bffa524badcbef2fba1a8254
KatisoTheGreat <katisoledwaba@gmail.com>
2023-06-04 18:39:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     return(
         <div>
         <Navbar />
-        {/* <Hero /> */}
+        <Hero />
         <section className="cards-list">
                 {cards}
         </section>
